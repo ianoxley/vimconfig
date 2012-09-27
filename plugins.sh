03070a917614916a0f698512a7a015fc896a5dae
Ian Oxley <ijoxley@gmail.com>
2012-09-27 11:24:30 +0100

    Added jshint plugin

--- plugins.sh
+++ plugins.sh
@@ -23,3 +23,4 @@ git clone git@github.com:ianoxley/snipmate-snippets.git
 git clone git://github.com/vimoutliner/vimoutliner.git
 git clone git://github.com/mivok/vimtodo.git
 git clone https://github.com/pangloss/vim-javascript.git
+git clone https://github.com/walm/jshint.vim.git
